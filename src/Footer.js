5cda6b19f1935891a083d44818e941a714bacd91
Zykezyke <victoriazyke@gmail.com>
2024-04-16 14:41:19 +0800

    Added percentage

--- src/Footer.js
+++ src/Footer.js
@@ -14,15 +14,23 @@ function Footer({ items }) {
     0
   );
 
+  let percentage = (completedItem.length / items.length) * 100;
+
+  let totalPercentage = (totalCompleted / totalQuantity) * 100;
   return (
     <div className="App-footer bg-dark text-light py-3">
       <p className="mb-0">Items Listed: {items.length}</p>
       <br />
-      <p>Completed: {completedItem.length}.</p>
+      <p>Completed: {completedItem.length}</p>
+      <br />
+      <p>Completion: {percentage}%</p>
+
       <br />
       <p>Total Item Count: {totalQuantity}</p>
       <br />
       <p>Total Completed Count: {totalCompleted}</p>
+      <br />
+      <p>Total Completion: {totalPercentage}%</p>
     </div>
   );
 }
