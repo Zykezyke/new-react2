652960a27c3739c43eecf2edf4a64e29c56da378
Zykezyke <victoriazyke@gmail.com>
2024-04-16 14:22:22 +0800

    added total completed

--- src/Footer.js
+++ src/Footer.js
@@ -9,13 +9,20 @@ function Footer({ items }) {
     0
   );
 
+  let totalCompleted = completedItem.reduce(
+    (total, currentItem) => total + currentItem.quantity,
+    0
+  );
+
   return (
     <div className="App-footer bg-dark text-light py-3">
-      <p className="mb-0">
-        You have {items.length} unique items in your list and have already
-        completed {completedItem.length}. There are {totalQuantity} items in
-        total.
-      </p>
+      <p className="mb-0">Items Listed: {items.length}</p>
+      <br />
+      <p>Completed: {completedItem.length}.</p>
+      <br />
+      <p>Total Item Count: {totalQuantity}</p>
+      <br />
+      <p>Total Completed Count: {totalCompleted}</p>
     </div>
   );
 }
